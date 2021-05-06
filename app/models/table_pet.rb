f0195b118b3f95771ccce4bb7f1e310f4980f470
123 <cookie.twenty01@gmail.com>
2021-05-06 17:36:30 -0400

    first update

---
+++ app/models/table_pet.rb
@@ -0,0 +1,2 @@
+class TablePet < ApplicationRecord
+end
---
+++ test/models/table_pet_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TablePetTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
