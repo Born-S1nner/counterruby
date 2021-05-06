d0de9f751fe08c195db802b91384ff4aa5a41ab4
123 <cookie.twenty01@gmail.com>
2021-05-06 18:05:02 -0400

    remove pet_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_213308) do
+ActiveRecord::Schema.define(version: 2021_05_06_215549) do
+
+  create_table "pets_lists", force: :cascade do |t|
+    t.string "specie"
+    t.string "owner"
+    t.string "pet_name"
+    t.string "owned_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "table_pets", force: :cascade do |t|
     t.string "specie"
--- test/models/table_pet_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class TablePetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
